3a8209178da4494fc354b4729feccf0a8712df6e
David De Medeiros <d_demedeiros@hotmail.com>
2025-08-10 13:25:44 -0700

    Add Limits Checks

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -2,18 +2,40 @@ import React from 'react'
 import CompanionForm from "@/components/CompanionForm";
 import {auth} from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Image from 'next/image';
+import Link from "next/link";
+import {newCompanionPermissions} from "@/lib/actions/companion.actions";
 
 const NewCompanion = async() => {
     const { userId } = await auth();
     if(!userId) redirect('/sign-in');
+
+    const canCreateCompanion = await newCompanionPermissions();
+
+
     return (
-        <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-            <article className="w-full gap-4 flex flex-col">
-                <h1>
-                    Companion Builder
-                </h1>
-                <CompanionForm />
-            </article>
+        <main className="min-lg:w:1/3 min-md:w-2/3 items-center justify-center">
+            {canCreateCompanion ? (
+                <article className="w-full gap-4 flex flex-col">
+                    <h1>
+                        Companion Builder
+                    </h1>
+                    <CompanionForm />
+                </article>
+            ) : (
+                <article className="companion-limit">
+                    <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230} />
+                    <div className="cta-badge">
+                        <a href="/subscription">
+                        Upgrade your plan </a>
+                    </div>
+                    <h1>You Have Reached Your Limit</h1>
+                    <p>You have reached your companion limit. Upgrade to create more companion and premium features.</p>
+                    <Link href="/subscription" className="btn-primary w-full justify-center" >
+                        Upgrade My Plan
+                    </Link>
+                   </article>
+            )}
         </main>
     )
 }
